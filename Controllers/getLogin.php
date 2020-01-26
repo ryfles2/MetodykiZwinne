02888c7e89f7a91cb1c71d5de008ee275e369d0e
SloneckiS <szymon.slonecki@o2.pl>
2020-01-26 18:16:23 +0100

    update

--- Controllers/getLogin.php
+++ Controllers/getLogin.php
@@ -3,7 +3,7 @@ require_once "databaseConnector.php";
 
 $login = $_GET['login'];
 $password = $_GET['pass'];
-$selectErrors = "SELECT login,type FROM Users  WHERE login=\"$login\" AND password =\"$password\"";
+$selectErrors = "SELECT id,login,type FROM Users  WHERE login=\"$login\" AND password =\"$password\"";
 $result = $con->query($selectErrors);
 if ($result->num_rows > 0) {
     $Errors =[];
@@ -11,8 +11,15 @@ if ($result->num_rows > 0) {
 		array_push($Errors,$row);
     }
     echo json_encode($Errors);
+	$response = array(
+   'state' => '0'
+   );
+    echo json_encode($response);
 } else {
-    json_encode(array(„state”=>0));
+   $response = array(
+   'state' => '1'
+   );
+    echo json_encode($response);
 }
 
 ?>
